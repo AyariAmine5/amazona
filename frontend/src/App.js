6b0bc026da7e5aa8f3e5d4f00ee1512e26f51523
Ayari Amine <mohamedamine.ayari@esprit.tn>
2021-10-04 17:09:39 +0100

    order history

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen.js";
+import OrderHistoryScreen from "./screens/orderHistoryScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -43,10 +44,15 @@ function App() {
                   {userInfo.name} <i className="fa fa-caret-down"></i>{" "}
                 </Link>
                 <ul className="dropdown-content">
-                  <Link to="#signout" onClick={SignoutHandler}>
-                    {" "}
-                    Sign Out
-                  </Link>
+                  <li>
+                    <Link to="/orderhistory">Order History</Link>
+                  </li>
+                  <li>
+                    <Link to="#signout" onClick={SignoutHandler}>
+                      {" "}
+                      Sign Out
+                    </Link>
+                  </li>
                 </ul>
               </div>
             ) : (
@@ -63,6 +69,7 @@ function App() {
           <Route path="/payment" component={PaymentMethodScreen}></Route>
           <Route path="/placeorder" component={PlaceOrderScreen}></Route>
           <Route path="/order/:id" component={OrderScreen}></Route>
+          <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All rights reserved.</footer>
